e8051b8322566744120ab7aab490db0134bf93c3
דבורה זרביב <dzarbiv@g.jct.ac.il>
2021-04-21 10:29:17 +0300

    FINAL 03

--- src/unittests/GeometriesTest.java
+++ src/unittests/GeometriesTest.java
@@ -6,9 +6,8 @@ import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 class GeometriesTest {
     /**
      * Test method for {@link geometries.Geometries#findIntersections(primitives.Ray)}.
@@ -19,23 +18,20 @@ class GeometriesTest {
 
         // ============ Equivalence Partitions Tests ==============
         /**part of the geometries has intersections points*/
-        assertEquals("part of the geometries has intersections points", 2,
-                geometries.findIntersections(new Ray(new Point3D(1,0,-1), new Vector(0,0,1))).size());
+        assertEquals( 2, geometries.findIntersections(new Ray(new Point3D(1,0,-1), new Vector(0,0,1))).size()
+        ,"part of the geometries has intersections points");
 
         // =============== Boundary Values Tests ==================
         /**empty geometries collections*/
-        assertNull("empty geometries collections",
-                geometries.findIntersections(new Ray(new Point3D(0,1,0), new Vector(1,0,5))));
+        assertNull(geometries.findIntersections(new Ray(new Point3D(0,1,0), new Vector(1,0,5))),"empty geometries collections");
 
         geometries.add(new Plane(new Point3D(1,1,0), new Vector(0,0,1)));
         geometries.add(new Triangle(new Point3D(1,0,0), new Point3D(0,1,0), new Point3D(0,0,1)));
         geometries.add((Intersectable) new Sphere(1d, new Point3D(1, 0, 0)));
         /**each geometry does'nt have intersections points*/
-        assertNull("each geometry does'nt have intersections points",
-                geometries.findIntersections(new Ray(new Point3D(0,0,2), new Vector(0,-1,0))));
+        assertNull(geometries.findIntersections(new Ray(new Point3D(0,0,2), new Vector(0,-1,0))),"each geometry does'nt have intersections points");
         /**just one geometry has intersections point*/
-        assertEquals("just one geometry has intersections point", 1,
-                geometries.findIntersections(new Ray(new Point3D(0,5,-1), new Vector(0,0,1))).size());
+        assertEquals( 1, geometries.findIntersections(new Ray(new Point3D(0,5,-1), new Vector(0,0,1))).size(),"just one geometry has intersections point");
 
 
 
